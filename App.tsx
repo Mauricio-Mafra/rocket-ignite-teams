25dbcece92de6e5fa78913572252e03d5b59baf9
Maurício <mauricio.mafrapedreiro@gmail.com>
2025-03-18 15:13:36 -0300

    Configurado tema da aplicação

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import { Groups } from '@screens/Groups';
-
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+ 
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
 
