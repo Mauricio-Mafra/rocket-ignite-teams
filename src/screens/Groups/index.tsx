6dba61c0bd4df9c743af4d465e92ebd5eb05a90f
Maurício <mauricio.mafrapedreiro@gmail.com>
2025-03-30 12:49:02 -0300

    feat(single): ✨ -> Finalizada tela inicial de listagem de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Container} from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
+  //const[groups, setGroups] = useState<string[]>(['Taverna da Rocket', 'Taverna do Kelvin', 'Duty Failed']);
+  const[groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header/>
@@ -10,6 +18,25 @@ export function Groups() {
         title='Turmas' 
         subtitle='Jogue com a sua turma'
       />
+      
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <GroupCard 
+          title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() =>
+          <ListEmpty message='Que tal cadastrar a primeira turma?'/>
+        }
+      />
+
+      <Button
+        title='Criar nova turma'
+      />
+
     </Container>
   );
 }
