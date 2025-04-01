c61412f1e40a37f0e42d1ded1f9fe101ce1c501b
Maurício <mauricio.mafrapedreiro@gmail.com>
2025-04-01 16:37:55 -0300

    feat(multi): ✨ -> Implementada navegação entre telas

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { Groups } from '@screens/Groups';
-import { ThemeProvider } from 'styled-components';
-import theme from './src/theme';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
-import { Loading } from '@components/Loading';
+import { ThemeProvider } from 'styled-components';
 import { StatusBar } from 'react-native';
+
+import theme from './src/theme';
+import { Routes } from './src/routes'
+
+import { Loading } from '@components/Loading';
  
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -16,7 +18,7 @@ export default function App() {
         translucent
       />
 
-      { fontsLoaded ? <Groups /> : <Loading/>}
+      { fontsLoaded ? <Routes /> : <Loading/>}
     </ThemeProvider>
   );
 }
