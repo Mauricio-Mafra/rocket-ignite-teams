0a9858b9d83520c9aa4c4b82d5c0dab586b0b943
Maurício <mauricio.mafrapedreiro@gmail.com>
2025-04-01 14:49:13 -0300

    refactor(single): ♻️ -> Alterada tela inicial da aplicação

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Players } from '@screens/Players';
+import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
@@ -16,7 +16,7 @@ export default function App() {
         translucent
       />
 
-      { fontsLoaded ? <Players /> : <Loading/>}
+      { fontsLoaded ? <Groups /> : <Loading/>}
     </ThemeProvider>
   );
 }
